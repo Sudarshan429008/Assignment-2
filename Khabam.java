54d32fbd7a778bb10b745110cae32b7d5a27daac
sudarshan reddy k <sudarshan reddy k>
2016-02-10 16:21:36 -0600

    intial commit

---
+++ Khabam.java
@@ -0,0 +1,25 @@
+
+public class Khabam 
+{
+	public static void main(String[] args) 
+	{
+		int count = 0;
+		
+			for(int  i = 1; i  <  1000000; i  = i  +  1)
+			{
+				if(i % 9 == 0 )
+		{
+		
+
+			count= count+1 ;
+		}
+				
+		}
+	System.out.println(count);
+
+			}
+			}
+
+
+
+
